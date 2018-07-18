38390627734476d0dae1b67a5148fe8eeafeaf81
Natasha K <kapr.natasha@gmail.com>
2018-07-18 18:15:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,16 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre #we still want Ruby to generate a reader for us
+
+  GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre) #Book GENRES keeps track of all genres
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
